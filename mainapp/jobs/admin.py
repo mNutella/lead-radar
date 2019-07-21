b261bd763553acee16cacfd3d3d65890b0a4ece8
mNutella <netcore.click@gmail.com>
2019-07-21 19:45:10 +0500

    feat(backend|jobs): add 'Plan' table and add 'admin' panel implementation

--- mainapp/jobs/admin.py
+++ mainapp/jobs/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 
-from .models import Job, Employer, Company
+from .models import Company, Employer, Job, Plan
 
 
 @admin.register(Employer)
 class EmployerAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     pass
+
--- mainapp/jobs/models.py
+++ mainapp/jobs/models.py
@@ -10,18 +10,24 @@ class Employer(models.Model):
     email = models.EmailField(max_length=320, blank=True, unique=True)
     activated = models.BooleanField(default=False)
 
+
 class Company(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=300, blank=True)
 
+
 class Plan(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150, blank=True)
+    price = models.IntegerField(default=0)
+    days = models.IntegerField(default=30)
+
 
 class Job(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    location = models.ForeignKey('cities_light.City', on_delete=models.CASCADE)
+    location = models.ForeignKey("cities_light.City", on_delete=models.CASCADE)
     link_to_desc = models.URLField(max_length=2000)
     contact_email = models.ForeignKey(Employer, on_delete=models.CASCADE)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
