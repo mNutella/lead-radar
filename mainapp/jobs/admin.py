779aa5eedde0b48af7e2567041e5327c0d65f591
mNutella <netcore.click@gmail.com>
2019-07-22 12:45:25 +0500

    fix(backend|jobs): format 'admin.py' file

--- mainapp/jobs/admin.py
+++ mainapp/jobs/admin.py
@@ -5,11 +5,7 @@ from .models import Company, Employer, Job, Plan, Subscription
 
 @admin.register(Employer)
 class EmployerAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "email",
-        "activated",
-    )
+    list_display = ("id", "email", "activated")
     list_filter = ("activated",)
     search_fields = ("email",)
     ordering = ("email",)
@@ -17,24 +13,16 @@ class EmployerAdmin(admin.ModelAdmin):
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "name",
-    )
+    list_display = ("id", "name")
     search_fields = ("name",)
     ordering = ("name",)
 
 
 @admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "name",
-        "price",
-        "days",
-    )
+    list_display = ("id", "name", "price", "days")
     list_filter = ("name",)
-    search_fields = ("name", )
+    search_fields = ("name",)
     ordering = ("name",)
 
 
@@ -47,19 +35,21 @@ class SubscriptionAdmin(admin.ModelAdmin):
         "start_date",
         "end_date",
     )
-    list_filter = ("plan__name", "start_date",)
-    search_fields = ("plan__name", "employer__email",)
-    ordering = ("start_date", "end_date",)
+    list_filter = ("plan__name", "start_date")
+    search_fields = ("plan__name", "employer__email")
+    ordering = ("start_date", "end_date")
 
     def get_plan_name(self, obj):
         return obj.plan.name
-    get_plan_name.admin_order_field = 'plan'
-    get_plan_name.short_description = 'Plan'
+
+    get_plan_name.admin_order_field = "plan"
+    get_plan_name.short_description = "Plan"
 
     def get_employer_email(self, obj):
         return obj.employer.email
-    get_employer_email.admin_order_field = 'employer'
-    get_employer_email.short_description = 'Employer'
+
+    get_employer_email.admin_order_field = "employer"
+    get_employer_email.short_description = "Employer"
 
 
 @admin.register(Job)
@@ -75,27 +65,35 @@ class JobAdmin(admin.ModelAdmin):
         "approved",
         "created_date",
     )
-    list_filter = ("approved", "created_date",)
-    search_fields = ("contact_email__email", "title", "company__name", "location__display_name",)
+    list_filter = ("approved", "created_date")
+    search_fields = (
+        "contact_email__email",
+        "title",
+        "company__name",
+        "location__display_name",
+    )
     ordering = ("created_date",)
 
     def get_company_name(self, obj):
         return obj.company.name
-    get_company_name.admin_order_field = 'company'
-    get_company_name.short_description = 'Company'
+
+    get_company_name.admin_order_field = "company"
+    get_company_name.short_description = "Company"
 
     def get_contact_email(self, obj):
         return obj.contact_email.email
-    get_contact_email.admin_order_field = 'contact_email'
-    get_contact_email.short_description = 'Contact Email'
+
+    get_contact_email.admin_order_field = "contact_email"
+    get_contact_email.short_description = "Contact Email"
 
     def get_location(self, obj):
         return obj.location.region
-    get_location.admin_order_field = 'location'
-    get_location.short_description = 'Location'
+
+    get_location.admin_order_field = "location"
+    get_location.short_description = "Location"
 
     def get_plan_name(self, obj):
         return obj.plan.name
-    get_plan_name.admin_order_field = 'plan'
-    get_plan_name.short_description = 'Plan'
 
+    get_plan_name.admin_order_field = "plan"
+    get_plan_name.short_description = "Plan"
