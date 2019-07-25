2e49990036394be693a3e9b67fd675eea4de9e50
mNutella <netcore.click@gmail.com>
2019-07-25 18:25:43 +0500

    feat(frontend|lead-radar): add bootstrap to the 'Navbar' component

--- assets/js/app/lead-radar/components/Navbar/Navbar.js
+++ assets/js/app/lead-radar/components/Navbar/Navbar.js
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/post_job">Post</Link>
-        </li>
-        <li>
-          <Link to="/press">Press</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-toggleable-md navbar-expand-md fixed-top navbar-light bg-faded">
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <Link className="navbar-brand" to="/">Navbar</Link>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link active" to="/">Home</Link>
+          <Link className="nav-item nav-link" to="/post_job">Post</Link>
+          <Link className="nav-item nav-link" to="/about">About</Link>
+          <Link className="nav-item nav-link" to="/press">Press</Link>
+        </div>
+      </div>
     </nav>
   );
 };
