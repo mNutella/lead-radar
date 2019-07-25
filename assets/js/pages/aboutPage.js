887fd548bb3db4ba45244aca9277d678ff9057a9
mNutella <netcore.click@gmail.com>
2019-07-26 00:25:14 +0500

    feat(frontend|about): add bootstrap to the 'About' page

--- assets/js/pages/aboutPage.js
+++ assets/js/pages/aboutPage.js
@@ -5,39 +5,48 @@ import React from 'react';
 
 const About = () => {
   return (
-    <main>
-      <h1>About</h1>
-      <p>
-        Boooom is a curated list of leadership job opportunities for designers.
-        We want to help great designers connect with great companies.
-      </p>
-      <h3>A network of partners and sponsors</h3>
-      <p className="notes">
-        Our audience is primarily composed of designers looking for opportunities in
-        leadership and management roles. They are usually not in a hurry,
-        but will act fast if the perfect opportunity comes by.
-        New visitors come here mainly by reference from their peers or
-        organically through specialized design communities and media outlets.
-        Monthly, we post dozens of job opportunities in awesome companies from all over the world.
-
-        We foster partnerships with key communities, organizations and
-        events to help the advancement of the discipline.
-        By showing the value of design for business,
-        design management and leadership becomes a strategic asset for organizations.
-        Many leadership roles are filled off-market just by reference,
-        taking several months, just because there wasn’t an efficient way to get to this audience.
-        Not anymore.
-
-        Would like to discuss a sponsorship opportunity? Email us at partners@boooom.co.
-      </p>
-      <h3>Who are we?</h3>
-      <p>Boooom is run by Tiago Machado and João Alfaiate.</p>
-      <h3>Keep up to date</h3>
-      <p>We’re live on Twitter, LinkedIn and Facebook, but also publish an awesome newsletter.</p>
-      <h3>Keep up to date</h3>
-      <p>We’re live on Twitter, LinkedIn and Facebook, but also publish an awesome newsletter.</p>
-      <h3>Contact us</h3>
-      <p>mail@boooom.co</p>
+    <main className="about-container">
+      <div className="container">
+        <div className="header">
+          <h1 className="display-4">About</h1>
+          <h4>
+            Boooom is a curated list of leadership job opportunities for designers.
+            We want to help great designers connect with great companies.
+          </h4>
+        </div>
+        <div className="content">
+          <h3>A network of partners and sponsors</h3>
+          <p>
+            Our audience is primarily composed of designers looking for opportunities in
+            leadership and management roles. They are usually not in a hurry,
+            but will act fast if the perfect opportunity comes by.
+            New visitors come here mainly by reference from their peers or
+            organically through specialized design communities and media outlets.
+            Monthly, we post dozens of job opportunities in awesome companies from all over the world.
+          </p>
+          <p>
+            We foster partnerships with key communities, organizations and
+            events to help the advancement of the discipline.
+            By showing the value of design for business,
+            design management and leadership becomes a strategic asset for organizations.
+            Many leadership roles are filled off-market just by reference,
+            taking several months, just because there wasn’t an efficient way to get to this audience.
+            Not anymore.
+          </p>
+          <p>
+            Would like to discuss a sponsorship opportunity?
+            Email us at <a href="#">partners@lead-radar.co</a>.
+          </p>
+          <h3>Who are we?</h3>
+          <p>Boooom is run by Tiago Machado and João Alfaiate.</p>
+          <h3>Keep up to date</h3>
+          <p>We’re live on Twitter, LinkedIn and Facebook, but also publish an awesome newsletter.</p>
+          <h3>Keep up to date</h3>
+          <p>We’re live on Twitter, LinkedIn and Facebook, but also publish an awesome newsletter.</p>
+          <h3>Contact us</h3>
+          <a href="#">mail@lead-radar.co</a>
+        </div>
+      </div>
     </main>
   );
 };
