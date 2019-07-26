879f43b8ef8f441290ab8291e93bf191128046d0
mNutella <netcore.click@gmail.com>
2019-07-26 18:43:59 +0500

    feat(frontend): added a 'Jobs' page

---
+++ assets/js/pages/jobsPage.js
@@ -0,0 +1,23 @@
+// import PropTypes from 'prop-types';
+import React from 'react';
+// import { graphql } from 'react-apollo';
+
+
+const Jobs = () => {
+  return (
+    <main className="press-container">
+      <div className="container">
+        <div className="header">
+          {/* search input */}
+          <h1 className="display-4">Temp</h1>
+          <h4>
+            Temp.
+          </h4>
+        </div>
+        {/* infinity list of jobs */}
+      </div>
+    </main>
+  );
+};
+
+export default Jobs;
