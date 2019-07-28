a7b82a0a24d6e1f7aca76305aa25dde373531b3c
mNutella <netcore.click@gmail.com>
2019-07-28 13:43:00 +0500

    chore(frontend): changed the 'Navbar' size(half)

--- assets/js/app/lead-radar/components/Navbar/Navbar.js
+++ assets/js/app/lead-radar/components/Navbar/Navbar.js
@@ -9,21 +9,23 @@ const Navbar = ({ location }) => {
   const isLight = location.pathname === '/jobs';
 
   return (
-    <nav className={`navbar navbar-expand-md fixed-top ${isLight ? 'navbar-dark' : 'navbar-light'}  bg-faded`}>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <Link className="navbar-brand" to="/">Navbar</Link>
-      <div className="collapse navbar-collapse pl-3 rounded" id="main-navbar">
-        <div className="navbar-nav">
-          <Link className="nav-item nav-link" to="/">Home</Link>
-          <Link className="nav-item nav-link" to="/jobs">Jobs</Link>
-          <Link className="nav-item nav-link" to="/post_job">Post</Link>
-          <Link className="nav-item nav-link" to="/about">About</Link>
-          <Link className="nav-item nav-link" to="/press">Press</Link>
+    <div className="row">
+      <nav className={`navbar navbar-expand-md fixed-top ${isLight ? 'navbar-dark' : 'navbar-light'} col-sm-6 bg-faded`}>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <Link className="navbar-brand" to="/">Navbar</Link>
+        <div className="collapse navbar-collapse pl-3 rounded" id="main-navbar">
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link" to="/">Home</Link>
+            <Link className="nav-item nav-link" to="/jobs">Jobs</Link>
+            <Link className="nav-item nav-link" to="/post_job">Post</Link>
+            <Link className="nav-item nav-link" to="/about">About</Link>
+            <Link className="nav-item nav-link" to="/press">Press</Link>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
