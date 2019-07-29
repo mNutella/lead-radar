cd4074e9967d366b13eb2d5b078f9eb493ab0b15
mNutella <netcore.click@gmail.com>
2019-07-29 11:48:25 +0500

    feat(frontend): added mobile support

--- assets/js/pages/homePage.js
+++ assets/js/pages/homePage.js
@@ -14,11 +14,11 @@ const Home = () => {
       <div className="container-fluid h-100">
         <div className="container h-100">
           <div className="row h-100 justify-content-center align-content-center">
-            <div className="col align-self-end">
-              <h1 className="mb-4">FIND YOUR JOB</h1>
+            <div className="col-sm-6 align-self-end">
+              <h1 className="mb-4 display-4">FIND YOUR JOB</h1>
               <form>
                 <div className="form-group row">
-                  <div className="col-8">
+                  <div className="col-9">
                     <input
                       type="text"
                       className="form-control form-control-lg shadow p-3 bg-white rounded border-0"
@@ -26,29 +26,17 @@ const Home = () => {
                     />
                   </div>
                 </div>
-                <div className="form-group row mb-4">
-                  <div className="col-8">
-                    <select
-                      defaultValue="City"
-                      className="form-control form-control-lg shadow p-3 bg-white rounded border-0"
-                    >
-                      <option value="City" disabled>City</option>
-                      <option>1</option>
-                      <option>2</option>
-                    </select>
-                  </div>
-                </div>
                 <Button
                   route
                   link="/jobs"
-                  size={8}
+                  size={6}
                   classes={['btn-primary', 'lift', 'font-weight-bold', 'rounded-pill']}
                 >
                   Search
                 </Button>
               </form>
             </div>
-            <div className="col">
+            <div className="col-sm-6">
               <img className="img-fluid mb-4" src="https://www.kraken.com/_assets/files/2019-04/Advanced4.png" alt="finder" />
               <Button
                 route
