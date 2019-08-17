965c909ab77f09bda85e3378c6181e7811491b92
mNutella <netcore.click@gmail.com>
2019-08-17 23:45:16 +0500

    feat(frontend): added scroll hook to the 'Navbar' component and updated style

--- assets/js/app/lead-radar/components/Navbar/Navbar.js
+++ assets/js/app/lead-radar/components/Navbar/Navbar.js
@@ -1,30 +1,62 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useResponsive } from '../../../../utils';
 
 import './style.scss';
 
 
+const useScroll = () => {
+  const [isScrolling, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScrollPos = () => {
+      const lastPosition = window.scrollY;
+      if (lastPosition > 50) {
+        setScroll(true);
+      } else if (isScrolling) {
+        setScroll(false);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScrollPos);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollPos);
+    };
+  }, []);
+
+  return { scroll: isScrolling };
+};
+
 const Navbar = ({ location }) => {
-  const isLight = location.pathname === '/jobs';
+  const locations = ['/', '/jobs', '/post', '/about', '/press'];
+
+  if (!locations.includes(location.pathname)) {
+    return <div />;
+  }
+
+  const isJobs = location.pathname === '/jobs';
+  const navHelper = useScroll();
+  const size = useResponsive();
 
   return (
-    <div className="row">
-      <nav className={`navbar navbar-expand-md fixed-top ${isLight ? 'navbar-dark' : 'navbar-light'} col-sm-6 bg-faded`}>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <div className="collapse navbar-collapse pl-3 rounded" id="main-navbar">
-          <div className="navbar-nav">
-            <Link className="nav-item nav-link" to="/jobs">Jobs</Link>
-            <Link className="nav-item nav-link" to="/post">Post</Link>
-            <Link className="nav-item nav-link" to="/about">About</Link>
-            <Link className="nav-item nav-link" to="/press">Press</Link>
-          </div>
+    <nav className={`navbar navbar-expand-md fixed-top ${isJobs && !size.mobile ? 'navbar-dark' : 'navbar-light'} ${isJobs && !size.mobile ? 'col-md-6' : 'col-md-12'} ${navHelper.scroll ? 'bg-primary navbar-dark shadow' : ''} custom-nav bg-faded`}>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <Link className="navbar-brand" to="/">Navbar</Link>
+      <div className={`collapse navbar-collapse ${isJobs ? 'justify-content-right' : ''} pl-3 rounded ${!navHelper.scroll && size.mobile ? 'shadow bg-primary' : ''}`} id="main-navbar">
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link" to="/jobs">Jobs</Link>
+          <Link className="nav-item nav-link" to="/post">Post</Link>
+          <Link className="nav-item nav-link" to="/about">About</Link>
+          <Link className="nav-item nav-link" to="/press">Press</Link>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
