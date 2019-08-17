486eab3d9b23671ba5d23ce09c2536235f6829d6
mNutella <netcore.click@gmail.com>
2019-08-17 23:36:24 +0500

    feat(frontend): added resize and responsive hooks

--- assets/js/utils/mixins.js
+++ assets/js/utils/mixins.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
 
 export const withSuspense = (WrappedComponent, LoaderComponent) => props => (
@@ -7,4 +7,51 @@ export const withSuspense = (WrappedComponent, LoaderComponent) => props => (
   </Suspense>
 );
 
-export const another = () => {};
+export const useResize = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return { width, height };
+};
+
+export const useResponsive = () => {
+  const [isMobile, setMobile] = useState(false);
+  const [isTablet, setTablet] = useState(false);
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 768) {
+        setMobile(true);
+        setTablet(false);
+        setDesktop(false);
+      } else if (width >= 768 && width < 1024) {
+        setMobile(false);
+        setTablet(true);
+        setDesktop(false);
+      } else if (width >= 1024) {
+        setMobile(false);
+        setTablet(false);
+        setDesktop(true);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return { mobile: isMobile, tablet: isTablet, desktop: isDesktop };
+};
