44d7b63a114672fd4203e761f285fbbc1237de1b
mNutella <netcore.click@gmail.com>
2019-08-21 14:06:31 +0500

    feat: added an auto focus prop to components

--- assets/js/app/lead-radar/components/Input/Input.js
+++ assets/js/app/lead-radar/components/Input/Input.js
@@ -1,40 +1,56 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import './style.scss';
 
 
 const Input = ({
-  label, type, ph, defVal, children, classes, onChange,
-}) => (
-  <div className="form-group">
-    {label !== '' && (<label>{label}</label>)}
-    <input
-      type={type}
-      defaultValue={defVal}
-      className={`form-control ${classes.join(' ')}`}
-      placeholder={ph}
-      onChange={onChange}
-    />
-    {children}
-  </div>
-);
+  name, label, type, ph, defVal, focus, children, classes, onChange,
+}) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (focus) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div className="form-group">
+      {label !== '' && (<label>{label}</label>)}
+      <input
+        name={name}
+        type={type}
+        ref={inputRef}
+        defaultValue={defVal}
+        className={`form-control ${classes.join(' ')}`}
+        placeholder={ph}
+        onChange={onChange}
+      />
+      {children}
+    </div>
+  );
+};
 
 Input.defaultProps = {
+  name: '',
   label: '',
   ph: '',
   type: 'text',
   defVal: '',
+  focus: false,
   children: undefined,
   classes: [],
   onChange: undefined,
 };
 
 Input.propTypes = {
+  name: PropTypes.string,
   label: PropTypes.string,
   ph: PropTypes.string,
   type: PropTypes.string,
   defVal: PropTypes.string,
+  focus: PropTypes.bool,
   // eslint-disable-next-line react/forbid-prop-types
   children: PropTypes.any,
   // eslint-disable-next-line react/forbid-prop-types
--- assets/js/app/lead-radar/components/Search/Search.js
+++ assets/js/app/lead-radar/components/Search/Search.js
@@ -1,40 +1,54 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Icon from '../Icon';
 
 import './style.scss';
 
 
 const Search = ({
-  defVal, ph, found, onChange,
-}) => (
-  <div className="input-group input-group-lg rounded shadow">
-    <div className="input-group-prepend">
-      <span className="input-group-text border-0 pr-1 bg-white">
-        <Icon name="search" size={22} />
-      </span>
-    </div>
-    <input
-      type="text"
-      aria-label="Search jobs"
-      defaultValue={defVal}
-      placeholder={ph}
-      className="form-control border-0 px-1"
-      onChange={onChange}
-    />
-    {found !== 0 && (
-      <div className="input-group-append">
-        <span className="input-group-text bg-white border-0 pl-1">
-          <span className="h6 text-uppercase text-right text-muted d-none d-md-block mb-0">
-            {found} найдено
-          </span>
+  focus, defVal, ph, found, onChange,
+}) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (focus) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div className="input-group input-group-lg rounded shadow">
+      <div className="input-group-prepend">
+        <span className="input-group-text border-0 pr-1 bg-white">
+          <Icon name="search" size={22} />
         </span>
       </div>
-    )}
-  </div>
-);
+      <input
+        type="text"
+        ref={inputRef}
+        aria-label="Search jobs"
+        defaultValue={defVal}
+        placeholder={ph}
+        className="form-control border-0 px-1"
+        onChange={onChange}
+      />
+      {found !== 0 && (
+        <div className="input-group-append">
+          <span className="input-group-text bg-white border-0 pl-1">
+            <span className="h6 text-uppercase text-right text-muted d-none d-md-block mb-0">
+              {found}
+              {' '}
+              найдено
+            </span>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
 
 Search.defaultProps = {
+  focus: false,
   defVal: '',
   ph: '',
   found: 0,
@@ -42,6 +56,7 @@ Search.defaultProps = {
 };
 
 Search.propTypes = {
+  focus: PropTypes.bool,
   defVal: PropTypes.string,
   ph: PropTypes.string,
   found: PropTypes.number,
