eaf056d16a81ca0d2340fc3d0f9e3e09a6550858
mNutella <netcore.click@gmail.com>
2019-08-30 16:46:23 +0500

    🛠️refactor(frontend): removed unused files

--- assets/js/constants/index.js
+++ assets/js/constants/index.js
@@ -1,4 +1,2 @@
 export { default as CITIES } from './cities';
-export { default as REGIONS } from './regions';
-export { default as CITIES_LIGHT } from './cities_light';
 export * from './constants';
