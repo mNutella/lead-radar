cd7884937623c351dc77cca3e0a3b2a702f6ca83
mNutella <netcore.click@gmail.com>
2019-08-30 15:54:40 +0500

    📦feat(frontend): added cities and regex

---
+++ assets/js/constants/constants.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-useless-escape */
+export const validUrlRegex = RegExp(
+  /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,
+);
+export const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+);
---
+++ assets/js/constants/index.js
@@ -0,0 +1,4 @@
+export { default as CITIES } from './cities';
+export { default as REGIONS } from './regions';
+export { default as CITIES_LIGHT } from './cities_light';
+export * from './constants';
